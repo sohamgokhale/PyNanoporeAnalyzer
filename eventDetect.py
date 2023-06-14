80e659920a02b8c70c210b0a2a03d172fdc18155
sohamgokhale <sohamgokhale26@gmail.com>
2023-06-14 20:57:42 +0100

    Updated Event class

--- eventDetect.py
+++ eventDetect.py
@@ -1,14 +1,49 @@
 import numpy as np
 
 class Event:
+    dataRate = 1
     def __init__(self) -> None:
-        self.eventStartIndex = 0
-        self.eventEndIndex = 0
+        self.startIndex = 0
+        self.endIndex = 0
         self.eventDuration = 0
         self.eventMaxima = 0
         self.eventMaximaIndex = 0
         self.eventDwellTime = 0
         self.event = np.empty
+        self.eventRiseTime = 0
+        self.eventFallTime = 0
+        self.integral = 0
+
+    def _calculateDwellTime(self):
+        print(str(self.event) + " : " + str(self.startIndex) +  " : " + str(np.size(self.event,0)))
+        dwellStart = self.eventMaximaIndex
+        dwellEnd = self.eventMaximaIndex
+        while ((self.event[dwellStart]>(self.eventMaxima/2)) and (dwellStart>1)):
+            dwellStart -= 1
+        while ((self.event[dwellEnd]>(self.eventMaxima/2)) and (dwellEnd < (np.size(self.event,0) - 1))):
+            dwellEnd += 1
+        self.eventDwellTime = (dwellEnd - dwellStart) * Event.dataRate
+
+    def _calculateRiseFallTime(self):
+        self.eventRiseTime = (self.eventMaximaIndex) * Event.dataRate
+        self.eventFallTime = (self.endIndex - self.eventMaximaIndex)
+
+    def _calculateIntegral(self):
+        self.integral = np.sum(self.event)
+
+    def extractFeatures(self):
+        if ((self.startIndex == 0) or (self.endIndex == 0) or (self.eventDuration == 0) or (self.eventMaxima == 0)):
+            print(self.startIndex)
+            print(self.endIndex)
+            print(self.eventDuration)
+            print(self.eventMaxima)
+            raise Exception("Could not extract event features")
+        else:
+            self._calculateDwellTime()
+            self._calculateRiseFallTime()
+            self._calculateIntegral()
+
+    
        
 class EventDetect:
     def __init__(self) -> None:
@@ -29,9 +64,11 @@ class EventDetect:
                 while data[self.peakEnd]>0:
                     self.peakEnd += 1
                 
-                if (self.peakEnd>self.peakStart):
+                if (self.peakEnd>self.peakStart+2):
                     e = Event()
                     e.event = data[self.peakStart:self.peakEnd]
+                    e.startIndex = self.peakStart
+                    e.endIndex = self.peakEnd
                     e.eventMaximaIndex = np.argmax(data[self.peakStart:self.peakEnd])
                     e.eventMaxima = data[self.peakStart + e.eventMaximaIndex]
                     e.eventDuration = self.peakEnd - self.peakStart
--- test.py
+++ test.py
@@ -4,17 +4,37 @@ from dataLoader import DataLoader
 from baselineDetect import BaselineMovMean
 from eventDetect import Event, EventDetect
 
-fig, (ax1, ax2) = plt.subplots(2)
-fig.suptitle('Vertically stacked subplots')
-
-data = DataLoader("sample.abf").getChannel(0)
+data = DataLoader("Data_interim/Linear_DNA_PBS_01.abf").getChannel(0)
 mean = BaselineMovMean().run(data,1000)
-ax1.plot(data[0,:],data[1,:])
-ax1.plot(data[0,:],mean)
 baselineSub = np.array([data[0,:],data[1,:]-mean])
 flipped = np.array([baselineSub[0,:], baselineSub[1,:] * -1])
 events, peaks, times = EventDetect().run(flipped,20)
-ax2.plot(data[0,:],flipped[1,:])
-ax2.plot(data[0,:],peaks)
-ax2.plot(data[0,:],times)
+
+plt.subplot(2,2,1)
+plt.plot(data[0,:],flipped[1,:])
+plt.plot(data[0,:],peaks)
+plt.plot(data[0,:],times)
+
+dwellTimes = []
+Amplitudes = []
+RiseTimes = []
+FallTimes = []
+Integrals = []
+for event in events:
+    event.extractFeatures()
+    dwellTimes.append(event.eventDwellTime)
+    Amplitudes.append(event.eventMaxima)
+    RiseTimes.append(event.eventRiseTime)
+    FallTimes.append(event.eventFallTime)
+    Integrals.append(event.integral)
+
+plt.subplot(2,2,2)
+plt.scatter(dwellTimes,Amplitudes)
+
+plt.subplot(2,2,3)
+plt.scatter(RiseTimes,FallTimes)
+
+plt.subplot(2,2,4)
+plt.hist(Integrals)
+
 plt.show()
