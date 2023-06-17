1604cddaf6d833c4d259c52436680805285bac2e
sohamgokhale <sohamgokhale26@gmail.com>
2023-06-17 23:12:22 +0100

    Added comments

--- filters.py
+++ filters.py
@@ -1,5 +1,38 @@
+"""
+File    : filter
+Author  : Soham Gokhale - UoL MSc Individual Project
+
+Description:
+------------
+"""
+
+from abc import ABC, abstractmethod
+from scipy.signal import butter, filtfilt
+import numpy as np
 
 
-class Filter:
-    # To be implemented later
-    pass
+class _Filter(ABC):
+    """ Private Abstract class for Basline Detection """
+    @abstractmethod
+    def run():
+        pass
+
+
+class ButterworthLPF(_Filter):
+    """
+    Apply N-th order Butterworth Low Pass Filter to remove high frequency
+    noise components from the data.
+    """
+
+    """ Setup Butterworth filter by calculating coefficients """
+
+    def __init__(self, order: int, cutoff: float, samplingFreq: float) -> None:
+        self.order = order
+        self.cutoff = cutoff
+        self.fs = samplingFreq
+        self.b, self.a = butter(order, cutoff, btype='low', fs=samplingFreq)
+
+    """ Apply Butterworth low pass filter to data using calculated coefficients """
+
+    def run(self, input: np.array) -> np.array:
+        return filtfilt(self.b, self.a, input)
