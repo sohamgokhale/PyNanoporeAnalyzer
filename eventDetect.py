ef9078f067866b791e348c83cbfa172009819036
sohamgokhale <sohamgokhale26@gmail.com>
2023-06-20 16:04:03 +0100

    Added EventCollector class

--- eventDetect.py
+++ eventDetect.py
@@ -96,18 +96,56 @@ class Event:
 
 
 """
+class EventCollect
+
+Description:
+------------
+Contains class EventCollect used as container for storing data associated
+with translocation event detected within EventDetect block. The class also
+stores data related to events and unpacks the events into various useful 
+arrays. The __init__ function is used for setup and configuration and the
+unpack() function for calculating event features.
+
+"""
+
+
+class EventCollector:
+    def __init__(self) -> None:
+        self.eventList = []
+        self.peaks = []
+        self.times = []
+        self.dwellTimes = []
+        self.Amplitudes = []
+        self.RiseTimes = []
+        self.FallTimes = []
+        self.Integrals = []
+        self.eventCount = 0
+
+    def unpack(self) -> None:
+        for event in self.eventList:
+            event.extractFeatures()
+            self.dwellTimes.append(event.eventDwellTime)
+            self.Amplitudes.append(event.eventMaxima)
+            self.RiseTimes.append(event.eventRiseTime)
+            self.FallTimes.append(event.eventFallTime)
+            self.Integrals.append(event.integral)
+
+
+"""
 class EventDetect
 
 Description:
 ------------
 Contains class EventDetect used for translocation event detection within
 signal. The __init__ function is used for setup and configuration and the
-run() function for execution. The class creates a list of 'Events'.
+run() function for execution. The class returns 'EventCollector' object.
 
 """
 
+
 class EventDetect:
-    _samplingTime = 1
+
+    """ Initialize object fields"""
 
     def __init__(self, samplingTime, _threshold) -> None:
         self._count = 0
@@ -118,34 +156,48 @@ class EventDetect:
             self._threshold = 0
         else:
             self._threshold = _threshold
-        EventDetect._samplingTime = samplingTime
+        self._samplingTime = samplingTime
 
-    def run(self, input: np.array) -> list:
-        data = input
-        iterator = 0
-        self._peaks = np.zeros(data.shape)
-        self._times = np.zeros(data.shape)
+    """ Run Method to detect events within signal array passed """
 
+    def run(self, input: np.array) -> EventCollector:
+
+        iterator = 0                            # Start Iterator at zero
+        # Create Empty arrays for storing
+        self._peaks = np.zeros(input.shape)
+        self._times = np.zeros(input.shape)      # Event Peaks and Event Times
+        ec = EventCollector()                   # Initialise EventCollector
+
+        # iterate through input
         while (iterator < input.size):
-            if (data[iterator] >= self._threshold):
+
+            # Collect Event if Amplitude crosses threshold
+            if (input[iterator] >= self._threshold):
 
                 self._peakStart = iterator
                 self._peakEnd = iterator
 
-                while data[self._peakStart] > 0:
+                # backtrack to start of event
+                while input[self._peakStart] > 0:
                     self._peakStart -= 1
 
-                while data[self._peakEnd] > 0:
+                # move forward to find end of event
+                while input[self._peakEnd] > 0:
                     self._peakEnd += 1
 
+                # collect various event details
                 if (self._peakEnd > iterator):
-                    e = Event(EventDetect._samplingTime)
-                    e.event = data[self._peakStart:self._peakEnd]
+
+                    e = Event(self._samplingTime)
+                    e._samplingTime = self._samplingTime
+                    e.event = input[self._peakStart:self._peakEnd]
                     e.startIndex = self._peakStart
                     e.endIndex = self._peakEnd
+
                     e.eventMaximaIndex = np.argmax(
-                        data[self._peakStart:self._peakEnd])
-                    e.eventMaxima = data[self._peakStart + e.eventMaximaIndex]
+                        input[self._peakStart:self._peakEnd])
+                    
+                    e.eventMaxima = input[self._peakStart + e.eventMaximaIndex]
                     e.eventDuration = self._peakEnd - self._peakStart
 
                     self._peaks[self._peakStart +
@@ -153,9 +205,20 @@ class EventDetect:
                     self._times[self._peakStart:self._peakEnd] = -10
 
                     self._count += 1
+
+                    # Start detecting new event from end of current Event
                     iterator = self._peakEnd
+
+                    # Append detected event to list
                     self._eventList.append(e)
+
             iterator += 1
-        e._samplingTime = EventDetect._samplingTime
-        print(self._count)
-        return self._eventList, self._peaks, self._times
+        
+        # Collect event details in EventCollector Object
+        ec.eventList = self._eventList
+        ec.peaks = self._peaks
+        ec.times = self._times
+        ec.eventCount = self._count
+        ec.unpack()
+        
+        return ec
