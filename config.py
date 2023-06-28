800396db4695bfa8eb15bde1e3efb6d3a016d7ca
sohamgokhale <sohamgokhale26@gmail.com>
2023-06-28 22:44:20 +0100

    Added custom colour palette

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import matplotlib as mpl
 from Blocks import baseline, event, load_data, filters, signal_utils, visualiser
 # Mapping between block types and their corresponding classes
 block_classes = {
@@ -12,5 +13,28 @@ block_classes = {
     'FFTPlot': visualiser.FFTPlot,
     'Histogram': visualiser.Histogram,
     'SigFFT': signal_utils.sigFFT,
-    'DensityPlot': visualiser.DensityPlot
+    'DensityPlot': visualiser.DensityPlot,
+    'ContourPlot': visualiser.ContourPlot
 }
+
+
+# Colourblind barrier-free palette - 
+# Masataka Okabe, Kei Ito (2008) [https://jfly.uni-koeln.de/color/]
+
+colors = {
+    'blue': (0, 114, 178),
+    'orange': (230, 159, 0),
+    'sky_blue': (86, 180, 233),
+    'green': (0, 158, 115),
+    'yellow': (240, 228, 66),
+    'red': (213, 94, 0),
+    'purple': (204, 121, 167),
+    'black': (0, 0, 0)
+}
+
+# Convert the RGB values to the range [0, 1]
+colors = {name: (r / 255, g / 255, b / 255)
+          for name, (r, g, b) in colors.items()}
+
+# Set the color cycle for line and marker colors
+mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=colors.values())
--- main.py
+++ main.py
@@ -1,45 +1,48 @@
-import utils, config
+import utils
+import config
 import argparse
 import matplotlib.pyplot as plt
+
 
 def setup(blockConfig):
     blocks = {}
     for blockData in blockConfig:
         blockName = blockData.get('name')
         blockClass = blockData.get('class')
-        blockParams = utils.decodeParams(blockData,blocks)
+        blockParams = utils.decodeParams(blockData, blocks)
 
         if blockClass in config.block_classes:
             blockCls = config.block_classes[blockClass]
             blockInstance = blockCls(*blockParams)
-            blocks.update({blockName:blockInstance})
+            blocks.update({blockName: blockInstance})
         else:
             print("Error in Input File")
     return blocks
 
-def run(blockConfig,blocks):
-    outputs ={}
+
+def run(blockConfig, blocks):
+    outputs = {}
     for blockData in blockConfig:
         inputList = []
         if blockData.get('type') != 'Source':
-            inputList = utils.decodeInputs(blockData,blocks,outputs)
+            inputList = utils.decodeInputs(blockData, blocks, outputs)
             block_instance = blocks.get(blockData.get('name'))
             output = block_instance.run(*inputList)
-            outputs.update({blockData.get('name'):output})
+            outputs.update({blockData.get('name'): output})
 
     plt.show()
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", type= str, help="Path to input file (yaml)", required=True)
+    parser.add_argument("-f", "--file", type=str,
+                        help="Path to input file (yaml)", required=True)
     args = parser.parse_args()
 
     blockConfig = utils.ParseYAML(args.file)
     blocks = setup(blockConfig)
-    run(blockConfig,blocks)
+    run(blockConfig, blocks)
 
 
 if __name__ == "__main__":
     main()
-    
-
