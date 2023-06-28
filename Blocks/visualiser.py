4986c65ae8dafe8d1faafcf77ce1b8bdf173ce94
sohamgokhale <sohamgokhale26@gmail.com>
2023-06-28 16:26:54 +0100

    Added more visualiser blocks

--- Blocks/visualiser.py
+++ Blocks/visualiser.py
@@ -1,6 +1,72 @@
 import matplotlib.pyplot as plt
+from Blocks import signal_utils
+import numpy as np
+from scipy.stats import kde
 
-class Scatterplot:
+class Figure:
+    def __init__(self) -> None:
+        self.figOpt = {}
+    
+    def decodeOptions(self):
+        for opt, val in self.figOpt.items():
+            try:
+                getattr(plt, opt)(val)
+            except:
+                print(f"Warning: Unknown option '{opt}'")
+
+
+class Scatterplot(Figure):
+    def __init__(self,figure_options : dict) -> None:
+        self.figOpt = figure_options
+
     def run(self,xAxis,yAxis) -> None:
+        plt.figure()
         plt.scatter(xAxis,yAxis)
-        plt.show()
+        self.decodeOptions()
+
+class TimePlot(Figure):
+    def __init__(self,figure_options : dict) -> None:
+        self.figOpt = figure_options
+
+    def run(self,xAxis,yAxis) -> None:
+        plt.figure()
+        plt.plot(xAxis,yAxis)
+        self.decodeOptions()
+
+class FFTPlot(Figure):
+    def __init__(self,samplingTime, figure_options : dict) -> None:
+        self.fft = signal_utils.sigFFT(samplingTime)
+        self.figOpt = figure_options
+
+    def run(self,signal) -> None:
+        plt.figure()
+        xAxis, yAxis = self.fft.run(signal)
+        plt.plot(xAxis,yAxis)
+        self.decodeOptions()
+
+class Histogram(Figure):
+    def __init__(self,binSize, figure_options : dict) -> None:
+        self.binSize = binSize
+        self.figOpt = figure_options
+
+    def run(self,data) -> None:
+        plt.figure()
+        plt.hist(data, bins=np.arange(min(data), max(data) + self.binSize, self.binSize))
+        self.decodeOptions()
+
+class DensityPlot(Figure):
+    def __init__(self, resolution, figure_options : dict) -> None:
+        self.bins = resolution
+        self.figOpt = figure_options
+        
+    def run(self, xAxis, yAxis):
+        xAxis = np.array(xAxis)
+        yAxis = np.array(yAxis)
+        k = kde.gaussian_kde([xAxis , yAxis])
+        xi, yi = np.mgrid[xAxis.min():xAxis.max():self.bins*1j,yAxis.min():yAxis.max():self.bins*1j]
+        zi = k(np.vstack([xi.flatten(), yi.flatten()]))
+
+        plt.figure()
+        plt.pcolormesh(xi, yi, zi.reshape(xi.shape), shading='auto',cmap=plt.cm.Blues)
+        plt.colorbar()
+        self.decodeOptions()
